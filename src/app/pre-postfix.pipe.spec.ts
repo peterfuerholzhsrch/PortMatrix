bbf829f6f00412580b26a74eb631bfa950596d48
Peter Fuerholz <peter.fuerholz@hsr.ch>
2017-03-26 21:55:59 +0200

    Tests added to pre-postfix.pipe.spec.ts

--- src/app/pre-postfix.pipe.spec.ts
+++ src/app/pre-postfix.pipe.spec.ts
@@ -5,4 +5,29 @@ describe('PrePostfixPipe', () => {
     const pipe = new PrePostfixPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('empty', () => {
+    const pipe = new PrePostfixPipe();
+    expect(pipe.transform(null, "aaa", "bbb")).toBe(null);
+    expect(pipe.transform('', "aaa", "bbb")).toBe('');
+  });
+
+  it('with value', () => {
+    const pipe = new PrePostfixPipe();
+    expect(pipe.transform(123, "a", "b")).toBe('a123b');
+    expect(pipe.transform('123', "a", "b")).toBe('a123b');
+  });
+
+  it('without prefix', () => {
+    const pipe = new PrePostfixPipe();
+    expect(pipe.transform(123, null, "b")).toBe('123b');
+    expect(pipe.transform('123', "", "b")).toBe('123b');
+  });
+
+  it('no postfix', () => {
+    const pipe = new PrePostfixPipe();
+    expect(pipe.transform(123, "a")).toBe('a123');
+    expect(pipe.transform('123', "a", '')).toBe('a123');
+  });
+
 });
